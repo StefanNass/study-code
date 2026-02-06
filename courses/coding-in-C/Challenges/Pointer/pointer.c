8e6f8adfbe82f1b753c83a9c296036725baaadcb
Stefan <stefan.nass@mein.gmx>
2026-02-06 14:49:36 +0100

    Challenge pointer redone

--- courses/coding-in-C/Challenges/Pointer/pointer.c
+++ courses/coding-in-C/Challenges/Pointer/pointer.c
@@ -2,10 +2,11 @@
 
 int main(){
     int myNumbers[5] = {10, 20, 30, 40, 50};
-    int *p1 = &myNumbers[0];
-    printf("Wert von an myNumbers der zweiten Stelle: %d\n", *(p1+1));
-    printf("Wert von myNumbers an der fuenften Stelle: %d\n", *(p1+4));
-    printf("Adresse von myNumbers an der zweiten Stelle: %p\n", (p1+1));
-    printf("Adresse von myNumbers an der fuenften Stelle: %p\n", (p1+4));
+    int *p1 = &myNumbers[1];
+    int *p2 = &myNumbers[4];
+    printf("Wert von an myNumbers der zweiten Stelle: %d\n", *p1);
+    printf("Wert von myNumbers an der fuenften Stelle: %d\n", *p2);
+    printf("Adresse von myNumbers an der zweiten Stelle: %p\n", p1);
+    printf("Adresse von myNumbers an der fuenften Stelle: %p\n", p2);
     return 0;
 }
