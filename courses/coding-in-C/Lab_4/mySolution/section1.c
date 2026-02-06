b9a2226727f62cc3efcbde8c4caffd1afc04ea07
Stefan <stefan.nass@mein.gmx>
2026-02-06 15:01:34 +0100

    Lab4 Section 1 done

---
+++ courses/coding-in-C/Lab_4/mySolution/section1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void changeSecondLetter(char *text) {
+    text[1] = 'c';
+}
+
+int main(){
+    char satz [] = "Curly bird catches the worm";
+    char *p1 = satz;
+    printf("Vor der Veraenderung:\n");
+    printf("%s\n", satz);
+    printf ("%s\n", p1);
+    *(p1 + 0) = 'E';
+    *(p1 + 1) = 'a';
+    printf("Nach der Veraenderung:\n");
+    printf("%s\n", p1);
+    char text[] = "AAtions speak louder than words";
+    changeSecondLetter(text);
+    printf(text);
+
+
+}
