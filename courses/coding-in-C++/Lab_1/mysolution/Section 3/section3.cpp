cb413bd39b988ada29cbcb7d326e85e94aa06486
Stefan <stefan.nass@mein.gmx>
2026-04-15 16:59:33 +0200

    Lab 1 Section 3 done

---
+++ courses/coding-in-C++/Lab_1/mysolution/Section 3/section3.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdint>
+
+namespace validation{
+    
+    bool isAdult (uint8_t age){
+        if (age >= 18){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    bool isSenior (uint8_t age){
+        if (age >= 65){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+int main(){
+    int tempAge;
+    bool isValid = false;
+
+    while(!isValid){
+        std::cout << "Please enter your age:" << std::endl;
+        if (!(std::cin >> tempAge)){
+            std::cout << "You did not enter a valid number. Please try again." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            continue;
+        }
+        else if (tempAge > 120 || tempAge < 0){
+            std::cout << "You did not enter a valid number. Please try again." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            continue;
+        }
+        else{
+            isValid = true;
+            uint8_t age = static_cast<uint8_t>(tempAge);
+            if (validation::isAdult(age) && validation::isSenior(age)){
+                std::cout << "You are " << static_cast<int>(age) << " years old." << std::endl;
+                std::cout << "That means you are a Senior." << std::endl;
+            }
+            else if (validation::isAdult(age) && !validation::isSenior(age)){
+                std::cout << "You are " << static_cast<int>(age) << " years old." << std::endl;
+                std::cout << "That means you are an adult." << std::endl;
+            }
+            else{
+                std::cout << "You are " << static_cast<int>(age) << " years old." << std::endl;
+                std::cout << "That means you are still a kid or teenager" << std::endl;
+            }
+        }
+    }
+    
+
+    return 0;
+}
